6d39da541069e6080cecd95567f0a30052ace504
Pratham Vishwakarma <zenither.pc0@gmail.com>
2024-05-09 20:17:44 +0530

    Modified main.c By Removing Unwanted Characters.

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * @author jelathro
- * @date 2/18/13
- * 
- * Print "Hello, World!" to the console
- */
-int main(void){
+@author jelathro
+@date 2/18/13
+  
+Print "Hello, World!" to the console
+*/
+
+int main(){
     printf("Hello, World!\n");
     return 0;
 }
